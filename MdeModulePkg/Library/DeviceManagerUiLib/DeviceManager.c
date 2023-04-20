18d71d577c86ec94e5c9d3385100b1309a9aebb8
Matt DeVillier <matt.devillier@gmail.com>
2023-04-20 13:10:23 -0500

    DeviceManager: Add empty lines between entries
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Change-Id: Idc23a60290e7c7ac5f79015e81b8523bc72dfb37

--- MdeModulePkg/Library/DeviceManagerUiLib/DeviceManager.c
+++ MdeModulePkg/Library/DeviceManagerUiLib/DeviceManager.c
@@ -650,6 +650,9 @@ CreateDeviceManagerForm (
             //
             if (!AddNetworkMenu) {
               AddNetworkMenu = TRUE;
+              // Always add an empty line before each entry
+              HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
+
               HiiCreateGotoOpCode (
                 StartOpCodeHandle,
                 NETWORK_DEVICE_LIST_FORM_ID,
@@ -664,6 +667,9 @@ CreateDeviceManagerForm (
             // In network device list form, same mac address device only show one menu.
             //
             while (AddItemCount > 0) {
+              // Always add an empty line before each entry
+              HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
+
               HiiCreateGotoOpCode (
                 StartOpCodeHandle,
                 NETWORK_DEVICE_FORM_ID,
@@ -684,6 +690,9 @@ CreateDeviceManagerForm (
               DevicePathId =  HiiSetString (HiiHandle, 0, DevicePathStr, NULL);
               FreePool (DevicePathStr);
             }
+
+            // Always add an empty line before each entry
+            HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
 
             HiiCreateGotoExOpCode (
               StartOpCodeHandle,
@@ -708,6 +717,9 @@ CreateDeviceManagerForm (
               DevicePathId =  HiiSetString (HiiHandle, 0, DevicePathStr, NULL);
               FreePool (DevicePathStr);
             }
+
+            // Always add an empty line before each entry
+            HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
 
             HiiCreateGotoExOpCode (
               StartOpCodeHandle,
