994ad0a4d9340e5f477802a775524b07b243310b
Matt DeVillier <matt.devillier@gmail.com>
2025-02-10 10:17:17 -0600

    UefiPayloadPkg/PlatformBootManager: clear screen after timeout
    Clear text from the screen after the key press timeout expires, before
    booting the default boot option.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
+++ UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
@@ -403,6 +403,11 @@ PlatformBootManagerWaitCallback (
   UINT16  TimeoutRemain
   )
 {
+  /* Clear text from screen once timeout expires */
+  if (TimeoutRemain == 0) {
+    gST->ConOut->ClearScreen (gST->ConOut);
+    BootLogoEnableLogo ();
+  }
   return;
 }
 
