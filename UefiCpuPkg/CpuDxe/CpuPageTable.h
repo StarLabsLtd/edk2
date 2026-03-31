735bd31b09dbe94fcef96cb73492f13ecdb1cb11
Matt DeVillier <matt.devillier@gmail.com>
2026-03-31 15:57:23 -0500

    UefiCpuPkg: Disable memory attributes protocol
    This reverts commit efaa102d006f242da52a17c81d824a5377da284b.
    
    Fixes booting from Ventoy.
    
    Continuous-integration-options: PatchCheck.ignore-multi-package
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- UefiCpuPkg/CpuDxe/CpuPageTable.h
+++ UefiCpuPkg/CpuDxe/CpuPageTable.h
@@ -9,7 +9,6 @@
 #pragma once
 
 #include <IndustryStandard/PeImage.h>
-#include <Protocol/MemoryAttribute.h>
 
 #define PAGE_TABLE_LIB_PAGING_CONTEXT_IA32_X64_ATTRIBUTES_PSE              BIT0
 #define PAGE_TABLE_LIB_PAGING_CONTEXT_IA32_X64_ATTRIBUTES_PAE              BIT1
@@ -151,15 +150,4 @@ GetPagingDetails (
   IN  PAGE_TABLE_LIB_PAGING_CONTEXT_DATA  *PagingContextData,
   OUT UINTN                               **PageTableBase     OPTIONAL,
   OUT UINT32                              **Attributes        OPTIONAL
-  );
-
-/**
-  Install Efi Memory Attribute Protocol.
-
-  @param Handle A pointer to the EFI_HANDLE on which the interface is to be installed
-
-**/
-VOID
-InstallEfiMemoryAttributeProtocol (
-  IN EFI_HANDLE  Handle
   );
