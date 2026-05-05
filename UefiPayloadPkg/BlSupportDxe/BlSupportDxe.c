294bdaa9c34ae0be0c2763dff815629494ef9436
Sean Rhodes <sean@starlabs.systems>
2026-05-05 22:08:56 +0100

    UefiPayloadPkg: Add TPM support
    Detect TPM-related ACPI tables, advertise the TPM instance to DXE,
    and enable the TPM1.2 and TPM2 drivers in UefiPayload.
    
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- UefiPayloadPkg/BlSupportDxe/BlSupportDxe.c
+++ UefiPayloadPkg/BlSupportDxe/BlSupportDxe.c
@@ -86,6 +86,7 @@ BlDxeEntryPoint (
   UINT32                     ThresholdH;
   UINT32                     ThresholdV;
   UINT32                     ViewportWidth;
+  UINTN                      Size;
 
   //
   // Find the frame buffer information and update PCDs
@@ -140,6 +141,27 @@ BlDxeEntryPoint (
     ASSERT_EFI_ERROR (Status);
     Status = PcdSet64S (PcdPciExpressBaseSize, AcpiBoardInfo->PcieBaseSize);
     ASSERT_EFI_ERROR (Status);
+
+    if (AcpiBoardInfo->TPM12Present)
+    {
+      Size = sizeof (gEfiTpmDeviceInstanceTpm12Guid);
+      Status = PcdSetPtrS (
+               PcdTpmInstanceGuid,
+               &Size,
+               &gEfiTpmDeviceInstanceTpm12Guid
+               );
+      ASSERT_EFI_ERROR (Status);
+    }
+    else if (AcpiBoardInfo->TPM20Present)
+    {
+      Size = sizeof (gEfiTpmDeviceInstanceTpm20DtpmGuid);
+      Status = PcdSetPtrS (
+                 PcdTpmInstanceGuid,
+                 &Size,
+                 &gEfiTpmDeviceInstanceTpm20DtpmGuid
+                 );
+      ASSERT_EFI_ERROR (Status);
+    }
   }
 
   Status = BlArchAdditionalOps (ImageHandle, SystemTable);
